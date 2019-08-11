3efcbccaf9fd14434d502e3b79e87f9b2be6cb05
Mohammed Alnadi <mohammed.alnadi@gmail.com>
2019-08-11 10:25:39 -0400

    Updates

---
+++ precache-manifest.1e8bf12be96ca052a90ae75a4558a9dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fc4876b72ff9128ec6e9ac81149a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b1e809fc3fbcdf9931",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c09eeed3ac75eb0cab9d",
+    "url": "/static/js/2.1ef43037.chunk.js"
+  },
+  {
+    "revision": "56b1e809fc3fbcdf9931",
+    "url": "/static/js/main.f9330e4d.chunk.js"
+  },
+  {
+    "revision": "c967259a3bb32cd85d80",
+    "url": "/static/js/runtime~main.cc858483.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
